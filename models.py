d556ef23d3cd6b0be3ccdb68fe2d53eca35027d8
Matthew Bomett <bomettmat2@gmail.com>
2024-02-13 21:02:31 +0300

    user model created

---
+++ models.py
@@ -0,0 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
+
+db = SQLAlchemy()
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=False)
+    phone_number = db.Column(db.String(12), nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.column(db.String, nullable=False)
+
