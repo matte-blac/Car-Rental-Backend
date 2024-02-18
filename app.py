16dd50e28d3b4d56418f6bf57a5e3cb555a92300
Kevin <kevinolongo9@gmail.com>
2024-02-18 13:37:01 +0300

    add hire end point

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from users import UsersResource
 from login import LoginResource, UserRegistrationResource
 from flask_jwt_extended import JWTManager
 from admin import AvailableCarResource,AdminAvailableCarResource
+from hire import AdminActionResource, HireResource, HireStatusResource
 
 # Create Flask application instance
 app = Flask(__name__)
@@ -36,6 +37,10 @@ api.add_resource(UserRegistrationResource, '/register')
 
 api.add_resource(AvailableCarResource, '/availablecars', '/availablecars/<int:availablecar_id>', '/public/availablecars')
 api.add_resource(AdminAvailableCarResource, '/availablecars/<int:availablecar_id>') 
+
+api.add_resource(HireResource, '/hire')
+api.add_resource(HireStatusResource, '/hire_status/<int:user_id>')
+api.add_resource(AdminActionResource, '/admin/action')
 
 # Start the Flask application if this script is executed directly
 if __name__ == '__main__':
